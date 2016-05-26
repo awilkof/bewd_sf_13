f840bd87d3004fe10c3f8369e2848c594fd43b43
awilkof <andrea@andreawilkof.com>
2016-05-25 18:44:03 -0700

    Notes updating.

--- 08_Ruby_Fundamentals/exercises/apartment.rb
+++ 08_Ruby_Fundamentals/exercises/apartment.rb
@@ -1,9 +1,10 @@
 require 'pry'
 require 'pry-byebug'
 
+#just the class, cookie cutter of the apartment spaces
 class Apartment
   attr_accessor :name, :url, :price, :neighborhood
-
+#Creates an instance of each of these qualities
   def initialize(name,url,price,neighborhood)
     @name = name
     @url = url
